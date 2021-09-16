c459e93a2c1aac51456b8481a5793e1793a92865
Ananyojha <76782360+Ananyojha@users.noreply.github.com>
2021-09-16 13:47:54 +0530

    Update and rename VPC to VPC.sh

--- Learn & earn/Security/VPC.sh
+++ Learn & earn/Security/VPC.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#! /bin/bash
 
 set -e
 
@@ -9,7 +9,7 @@ gcloud config set  compute/region us-central1
 
 cat<<EOF>mangu.sh
 
-#!/usr/bin/env bash
+#! /bin/bash
 
 sudo apt-get install nginx-light -y
 sed -i 's/Welcome to nginx!/Welcome to the blue server!/g' /var/www/html/index.nginx-debian.html
@@ -27,11 +27,12 @@ gcloud compute instances create green \
   --tags web-server \
   --metadata-from-file=startup-script=mangu.sh
 
-gcloud compute firewall-rules create allow-http-web-server \
-  --target-tags web-server \
-  --source-ranges 0.0.0.0/0 \
-  --rules TCP:80, icmp
+
+
+gcloud compute firewall-rules create allow-http-web-server --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=icmp,tcp:22 --source-ranges=0.0.0.0/0 --target-tags web-server \
+
+
 
 gcloud compute instances create test-vm --machine-type=f1-micro --subnet=default --zone=us-central1-a
 
-
+echo -e "now start from Create a service account " 
